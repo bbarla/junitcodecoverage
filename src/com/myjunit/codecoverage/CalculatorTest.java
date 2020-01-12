1f98298020c9f021738f49070f00ae8b9ebaaa84
bhaskar barla <bhaskar.r.barla@gmail.com>
2020-01-12 16:45:25 +0530

    test case

--- src/com/myjunit/codecoverage/CalculatorTest.java
+++ src/com/myjunit/codecoverage/CalculatorTest.java
@@ -1,6 +1,9 @@
 package com.myjunit.codecoverage;
 
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import com.math.calculator.Calculator;
@@ -16,9 +19,12 @@ public class CalculatorTest {
 	public void test() {
 		System.out.println("Starting Junit ...");
 		int i=0;int j = 0; int sum = 0;
+		boolean condition = false;
 		int sumc = c.add(i, j);
-		assertEquals(sum, sumc);
-
+		if(sum==sumc) {
+			condition = true;
+		} 
+		assertTrue(condition);
 	}
 
 }
